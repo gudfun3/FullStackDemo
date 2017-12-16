983de2aceadfe5f71dc903de7ce4167934549136
gudfun3 <gudfun3@gmail.com>
2017-12-16 09:30:51 +0530

    first Commit

---
+++ src/main/java/com/asu/FullStackDemo/FullStackDemoApplication.java
@@ -0,0 +1,12 @@
+package com.asu.FullStackDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/asu/FullStackDemo/FullStackDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.asu.FullStackDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullStackDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
